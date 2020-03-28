20cc83e8b74999e75c196077acdf2954d6698a44
Masha Padilla <mashapadilla@gmail.com>
2020-03-28 12:36:41 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,12 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
     });
+
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("toggle");
+    });
+   
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("toggle");
+    });
 });
